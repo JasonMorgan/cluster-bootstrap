c580985d6f55000cc3afa1879a9a16b50276bd36
jasonmorgan <jmorgan@f9vs.com>
2022-09-13 15:28:04 -0400

    swap to AES.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ if [ -z "${2}" ]
 then
   clusters=(test dev prod1)
 else
-  clusters=("${2}")
+  clusters=("${@:2}")
 fi
 
 case "${1}" in
@@ -29,23 +29,45 @@ case "${1}" in
       export KUBECONFIG=~/tmp/"${c}"
 
       ## Install Apps
+      ### Linkerd
       "${linkerd_install[@]}"
       /home/jason/.linkerd2/bin/linkerd-stable-2.11.4 check
+      
+      ### BCloud
+
       helm install --create-namespace --namespace buoyant-cloud  --values manifests/buoyant/values.yaml --set managed=true --set metadata.agentName="${c}" --set controlPlaneOperator.extendedRBAC.enabled=true linkerd-buoyant linkerd-buoyant/linkerd-buoyant
-      helm install front-door nginx-stable/nginx-ingress -f manifests/nginx-ingress/values.yaml -n nginx-ingress --create-namespace --wait
-      helm install cert-manager jetstack/cert-manager --create-namespace --namespace cert-manager -f manifests/cert-manager/values.yaml --wait
+      
+      ### AES
+      
+      kubectl apply -f https://app.getambassador.io/yaml/edge-stack/3.1.0/aes-crds.yaml
+      kubectl wait --timeout=90s --for=condition=available deployment emissary-apiext -n emissary-system
+      kubectl scale -n emissary-system deployment emissary-apiext --replicas=1
+      helm install -n ambassador --create-namespace edge-stack datawire/edge-stack --wait
+
+      ### Apps
+      
       curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/emojivoto.yml | linkerd inject - | kubectl apply -f -
-      kubectl create ns booksapp
-      curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/booksapp.yml | linkerd inject - | kubectl apply -n booksapp -f -
+      # kubectl create ns booksapp
+      # curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/booksapp.yml | linkerd inject - | kubectl apply -n booksapp -f -
       
       ## Create BCloud Config
-       if [[ "${c}" == "prod"* ]]
+      if [[ "${c}" == "prod"* ]]
       then
         kubectl apply -f manifests/buoyant/dataplane-prod.yaml
         kubectl apply -f manifests/buoyant/controlplane-prod.yaml
-      else
+      elif [[ "${c}" == "dev" || "${c}" == "test" ]]
+      then
         kubectl apply -f manifests/buoyant/dataplane.yaml
         kubectl apply -f "manifests/buoyant/controlplane-${c}.yaml"
+        # kubectl apply -f ../linkerd-demos/policy/manifests/booksapp
+        # kubectl annotate ns booksapp config.linkerd.io/default-inbound-policy=deny
+        # kubectl rollout restart deploy -n booksapp
+      else
+        kubectl apply -f manifests/buoyant/dataplane.yaml
+        kubectl apply -f "manifests/buoyant/controlplane-dev.yaml"
+        # kubectl apply -f ../linkerd-demos/policy/manifests/booksapp
+        # kubectl annotate ns booksapp config.linkerd.io/default-inbound-policy=deny
+        # kubectl rollout restart deploy -n booksapp
       fi
       unset KUBECONFIG
     }
