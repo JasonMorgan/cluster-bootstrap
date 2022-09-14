2d2251e8b17c842731ff858817b95d0229c23965
jasonmorgan <jmorgan@f9vs.com>
2022-09-14 08:41:11 -0400

    update skaffolding.

--- mc.sh
+++ mc.sh
@@ -13,22 +13,30 @@ for c in "${clusters[@]}"
 {
   # Load config
   civo k8s config "${c}" > ~/.kube/configs/"${c}"
-  chmod 600 ~/tmp/"${c}"
-  export KUBECONFIG=~/.kube/configs/"${c}"
+  chmod 600 ~/.kube/configs/"${c}"
+  # export KUBECONFIG=~/.kube/configs/"${c}"
+  civo k8s config "${c}" > ~/.kube/config
 
   ## Install Viz
-  helm install grafana -n grafana --create-namespace grafana/grafana -f https://raw.githubusercontent.com/linkerd/linkerd2/main/grafana/values.yaml --wait
-  linkerd viz install --set grafana.url=grafana.grafana:3000 | kubectl apply -f - && linkerd check
-  
+  helm install grafana -n grafana --create-namespace grafana/grafana -f https://raw.githubusercontent.com/linkerd/linkerd2/main/grafana/values.yaml --wait || true
+  linkerd viz install --set grafana.url=grafana.grafana:3000 | kubectl apply -f - || true
+  # linkerd check
+
   ## Install mc
-  linkerd multicluster install | k apply -f - && linkerd check
+  linkerd multicluster install | kubectl apply -f - || true
+  # linkerd check
 
   ## Failover
-  helm install linkerd-failover -n linkerd-failover --create-namespace linkerd/linkerd-failover
+  helm install linkerd-failover -n linkerd-failover --create-namespace linkerd/linkerd-failover || true
 
-  unset KUBECONFIG
+  ## Allow BCloud
+
+  kubectl apply -f manifests/finalizers/allow-bcloud.yaml
+
+  linkerd check
+  # unset KUBECONFIG
 }
-linkerd multicluster link  --kubeconfig ~/.kube/configs/west --cluster-name west | k apply --kubeconfig ~/.kube/configs/east -f -
-linkerd multicluster link  --kubeconfig ~/.kube/configs/east --cluster-name east | k apply --kubeconfig ~/.kube/configs/west -f -
+linkerd multicluster link  --kubeconfig ~/.kube/configs/west --cluster-name west | kubectl apply --kubeconfig ~/.kube/configs/east -f -
+linkerd multicluster link  --kubeconfig ~/.kube/configs/east --cluster-name east | kubectl apply --kubeconfig ~/.kube/configs/west -f -
 
 
---
+++ update-wildcard.sh
@@ -0,0 +1,27 @@
+#!/bin/env bash
+set -e
+
+if [[ -z "${2}" ]]
+then
+  domain="civo.59s.io"
+else
+  domain="${2}"
+fi
+
+if [[ -z "${1}" ]]
+then
+  echo "./update-wildcard.sh ipAddress [domain]"
+  exit 1
+else
+  ipAddress="${1}"
+fi
+
+#civo domain record remove civo.59s.io 914123f2-0981-47d1-97c4-546b56c610ea
+civo domain record ls civo.59s.io
+read -r -p "enter old wildcard record id: " id
+civo domain record delete civo.59s.io "${id}" -y
+civo domain record add "${domain}" -n '*' -e A -v "${ipAddress}"
+
+## Add finalizers to east
+
+kubectly apply --kubeconfig ~/.kube/configs/east -f manifests/finalizers/
